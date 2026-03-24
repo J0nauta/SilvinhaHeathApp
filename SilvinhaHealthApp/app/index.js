f866e933e4c8d52796be88493e3eb50218b8548b
J0nauta <jhonataromao2010@gmail.com>
2026-03-24 12:25:13 -0300

    seja criativo

---
+++ SilvinhaHealthApp/app/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { 
+    FlatList,
+    StyleSheet, 
+    Text,
+    TouchableOpacity, 
+    View
+  } from 'react-native';
+  import { SafeAreaView } from 'react-native-safe-area-context';
+  import { router } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
