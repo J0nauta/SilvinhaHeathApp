f358cce36c0741e8724cf63c102abe928ea84960
J0nauta <jhonataromao2010@gmail.com>
2026-04-07 12:25:52 -0300

    Commit

---
+++ SilvinhaHealthApp/app/_layout.js
@@ -0,0 +1,15 @@
+<Stack.Screen
+name="imc"
+options={{
+title: 'Calculadora de IMC',
+headerShown: true
+}}
+/>
+
+<Stack.Screen
+name="peso-ideal"
+options={{
+title: 'Peso Ideal',
+headerShown: true
+}}
+/>
