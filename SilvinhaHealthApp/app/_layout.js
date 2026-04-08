fd72f3456099bdd1e9976b77e20e6e1720ed734b
J0nauta <jhonataromao2010@gmail.com>
2026-04-08 11:37:21 -0300

    Projeto Completo

--- SilvinhaHealthApp/app/_layout.js
+++ SilvinhaHealthApp/app/_layout.js
@@ -1,15 +1,31 @@
-<Stack.Screen
-name="imc"
-options={{
-title: 'Calculadora de IMC',
-headerShown: true
-}}
-/>
+<>
+  <Stack.Screen
+    name="imc"
+    options={{
+      title: 'Calculadora de IMC',
+      headerShown: true
+    }}
+  />
 
-<Stack.Screen
-name="peso-ideal"
+  <Stack.Screen
+    name="peso-ideal"
+    options={{
+      title: 'Peso Ideal',
+      headerShown: true
+    }}
+  />
+  <Stack.Screen
+name="agua"
 options={{
-title: 'Peso Ideal',
+title: 'Água Diária',
 headerShown: true
 }}
 />
+<Stack.Screen
+name="tmb"
+options={{
+title: 'Taxa Metabólica Basal',
+headerShown: true
+}}
+/>
+</>
